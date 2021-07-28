759388398e5da600344b06e49a0b801294870662
Grant Orchard <go@hashicorp.com>
2021-07-28 14:17:14 +1000

    add tenant_id as a discrete output

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,10 @@ output "azuread_application_password" {
 	value = azuread_application_password.this.value
 }
 
+output "azuread_tenant_id" {
+	value = data.azuread_client_config.this.tenant_id
+}
+
 output "oidc_discovery_url" {
 	value = "https://login.microsoftonline.com/${data.azuread_client_config.this.tenant_id}/v2.0"
 }
